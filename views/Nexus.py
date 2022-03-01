69b2b89c4abdbecfbcaf0313098f1dd3e9c9b44d
justinfederico <jfederico0241@sdsu.edu>
2022-02-28 22:56:16 -0800

    Reorganized directories and added CSS and JS files for the home page, exported from Adobe XD. Also added required images (Nocturne pics).

--- views/Nexus.py
+++ views/Nexus.py
@@ -1,11 +1,13 @@
 from flask import Flask, redirect, url_for, render_template
 
-app = Flask(__name__)
+app = Flask(__name__,
+            static_folder='static',
+            template_folder='templates')
 
 #Defining routes for the site, directing the user to the desired page
 @app.route("/")
 def home():
-    return render_template("test.html", summonerName="Fed")
+    return render_template("homePage.html", summonerName="Fed")
 
 @app.route("/admin")
 def admin():
