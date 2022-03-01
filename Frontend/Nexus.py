4f6dcc520d719e5a5a2148641954745a7c99ce8f
justinfederico <jfederico0241@sdsu.edu>
2022-02-28 23:49:46 -0800

    Reorganized the directories to Flask defaults for ease of use, and got the Adobe XD HTML to run properly with CSS and JS functioning properly.

--- Frontend/Nexus.py
+++ Frontend/Nexus.py
@@ -3,9 +3,9 @@ from flask import Flask, redirect, url_for, render_template
 app = Flask(__name__)
 
 #Defining routes for the site, directing the user to the desired page
-@app.route("/")
+@app.route("/", methods=["POST", "GET"])
 def home():
-    return render_template("index.html", summonerName="Fed")
+    return render_template("homePage.html", summonerName="Fed")
 
 @app.route("/admin")
 def admin():
