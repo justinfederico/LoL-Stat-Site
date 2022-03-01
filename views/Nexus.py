3ad20b8e138435162313d120b32b67d22a13055d
Jarel Macanas <jcmacanas326@gmail.com>
2022-02-28 21:56:16 -0800

    Changed to run test.html

--- views/Nexus.py
+++ views/Nexus.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html", summonerName="Fed")
+    return render_template("test.html", summonerName="Fed")
 
 @app.route("/admin")
 def admin():
