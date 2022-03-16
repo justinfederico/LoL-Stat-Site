1cd1988117cffc7c2fcdb1c0add8e7eec2d8ec14
Jarel Macanas <jcmacanas326@gmail.com>
2022-03-16 05:04:15 -0700

    Finished all routes to and from each HTML webpages, and formatting navbar and main website layout.

--- Frontend/Nexus.py
+++ Frontend/Nexus.py
@@ -3,29 +3,29 @@ from flask import Flask, redirect, url_for, render_template
 app = Flask(__name__)
 
 #Defining routes for the site, directing the user to the desired page
-@app.route("/", methods=["POST", "GET"])
+@app.route("/logoclick", methods=["POST", "GET"])
+def logoclick():
+    return render_template("index.html")
+
+@app.route("/home", methods=["POST", "GET"])
 def home():
     return render_template("index.html")
 
-@app.route("/admin")
-def admin():
-    return redirect(url_for("/"))
-
-@app.route("/champions")
+@app.route("/champions", methods=["POST", "GET"])
 def champions():
-    return redirect(url_for("/"))
+    return render_template("champions.html")
 
 @app.route("/lookup", methods=["POST", "GET"])
 def lookup():
-    return render_template("summonerSearch.html")
+    return render_template("summonersearch.html")
 
-@app.route("/about")
+@app.route("/about", methods=["POST", "GET"])
 def about():
-    return redirect(url_for("/"))
+    return render_template("aboutus.html")
 
-@app.route("/support")
+@app.route("/support", methods=["POST", "GET"])
 def support():
-    return redirect(url_for("/"))
+    return render_template("support.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
