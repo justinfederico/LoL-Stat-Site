3a8d4c7b97b6acafc7765cf7a50953bb581856d5
justinfederico <jfederico0241@sdsu.edu>
2022-04-03 12:14:21 -0700

    Added responsive search bar and stylized it, and animated with some simple JS to hide and unhide the search bar. Still need to add region select, will do that later.

--- Frontend/static/js/summonersearch.js
+++ Frontend/static/js/summonersearch.js
@@ -1,1 +1,8 @@
-//Update Search Bar Dropdown Option
+//Update Search Bar Dropdown Option and animate click
+var btn = document.getElementById("btn");
+var input = document.getElementById("input");
+
+btn.addEventListener("click", function(){
+   this.classList.toggle("btn_active");
+   input.classList.toggle("input_active");
+})
